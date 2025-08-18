05b390ea0980d2f0b2d01043dd3b627ce94e1d90
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-08-18 14:14:25 +0200

    added methods for API endpoints to BookService.

--- backend/src/main/java/org/example/backend/service/BookService.java
+++ backend/src/main/java/org/example/backend/service/BookService.java
@@ -1,6 +1,7 @@
 package org.example.backend.service;
 
 import org.example.backend.model.Book;
+import org.example.backend.model.BookDTO;
 import org.example.backend.repo.BookRepo;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +20,30 @@ public class BookService {
 
     public List<Book> getAllBooks() {
         return bookRepo.findAll();
+    }
+
+    public Book getBookById(String id) {
+        return bookRepo.findById(id).orElse(null);
+    }
+
+    public Book addBook(BookDTO book) {
+        String newId = idService.randomId();
+        bookRepo.save(new Book(
+                newId, book.title(), book.author(), book.isbn(), book.description(), book.publicationDate()
+        ));
+        return bookRepo.findById(newId).orElse(null);
+    }
+
+    public Book updateBook(String id, BookDTO book) {
+        bookRepo.save(new Book(
+                id, book.title(), book.author(), book.isbn(), book.description(), book.publicationDate()
+        ));
+        return bookRepo.findById(id).orElse(null);
+    }
+
+    public Book deleteBook(String id) {
+        Book bookToDelete = bookRepo.findById(id).orElse(null);
+        bookRepo.deleteById(id);
+        return bookToDelete;
     }
 }
