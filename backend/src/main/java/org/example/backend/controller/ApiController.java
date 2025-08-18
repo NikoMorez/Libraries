f4c6f255a0551eb4fe0e4c12c417573089c7f380
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-08-18 14:04:16 +0200

    added missing API endpoints to ApiController

--- backend/src/main/java/org/example/backend/controller/ApiController.java
+++ backend/src/main/java/org/example/backend/controller/ApiController.java
@@ -2,10 +2,9 @@ package org.example.backend.controller;
 
 
 import org.example.backend.model.Book;
+import org.example.backend.model.BookDTO;
 import org.example.backend.service.BookService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,4 +23,23 @@ public class ApiController {
         return bookService.getAllBooks();
     }
 
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable String id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    public Book addBook(@RequestBody BookDTO book) {
+        return bookService.addBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable String id, @RequestBody BookDTO book) {
+        return bookService.updateBook(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public Book deleteBook(@PathVariable String id) {
+        return bookService.deleteBook(id);
+    }
 }
