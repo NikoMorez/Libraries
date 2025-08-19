777093161043c442c111a206310fe5386981d347
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-08-19 18:09:34 +0200

    changed date format from String to LocalDate

--- backend/src/main/java/org/example/backend/model/GoogleVolumeInfo.java
+++ backend/src/main/java/org/example/backend/model/GoogleVolumeInfo.java
@@ -1,7 +1,8 @@
 package org.example.backend.model;
 
+import java.time.LocalDate;
 import java.util.List;
 
-public record GoogleVolumeInfo(String title, String[] authors, String publishedDate, String description,
+public record GoogleVolumeInfo(String title, String[] authors, LocalDate publishedDate, String description,
                                List<GoogleIndustryIdentifier> industryIdentifiers, GoogleImageLinks imageLinks) {
 }
