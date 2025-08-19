791e1657030c1062bea2cbd57ca93d9544df2b7d
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-08-19 15:48:12 +0200

    Created GoogleRequestService and building a RestClient

---
+++ backend/src/main/java/org/example/backend/service/GoogleRequestService.java
@@ -0,0 +1,15 @@
+package org.example.backend.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class GoogleRequestService {
+
+    private final RestClient restClient;
+
+    public GoogleRequestService(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder
+                .baseUrl("https://www.googleapis.com/books/v1/volumes").build();
+    }
+}
