a65a2becd7aeca165fdefefe73996d082336ff0a
NeitzelJulia <julia.neitzel86@gmail.com>
2025-08-21 16:50:49 +0200

    add SecurityConfig

---
+++ backend/src/main/java/org/example/backend/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.example.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers( HttpMethod.GET,"/api/example").authenticated()
+                        .anyRequest().permitAll())
+                .oauth2Login(o -> o
+                        .defaultSuccessUrl("http://localhost:5173"));
+        return http.build();
+    }
+}
