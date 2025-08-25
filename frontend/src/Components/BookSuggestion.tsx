275be631db940602509641bb19e305d9a400f5a2
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-08-25 15:39:33 +0200

    grundfunktion, bilder über den post-endpunkt hinzuzufügen.

--- frontend/src/Components/BookSuggestion.tsx
+++ frontend/src/Components/BookSuggestion.tsx
@@ -1,10 +1,21 @@
 import type {Book} from "../models/Book.tsx";
+import type {BookDTO} from "../models/BookDTO.tsx"
+import axios from "axios";
 
 export default function BookSuggestion(Book : {BookItem:Book}) {
 
     function truncateText(text:string, maxLength:number) {
         if (!text) return '';
         return text.length > maxLength ? text.slice(0, maxLength) + '...' : text;
+    }
+
+    function addBookToDB() {
+        const bookToPost:BookDTO = {title:Book.BookItem.title, author:Book.BookItem.author, isbn:Book.BookItem.isbn,
+            categories:Book.BookItem.categories, publicationDate:Book.BookItem.publicationDate,
+            description:Book.BookItem.description, smallThumbnail:Book.BookItem.smallThumbnail,
+            thumbnail:Book.BookItem.thumbnail}
+        axios.post("/api/books", bookToPost).then((res) => {console.log(res.data);})
+            .catch(e => {console.log(e);});
     }
 
     return (
@@ -19,6 +30,7 @@ export default function BookSuggestion(Book : {BookItem:Book}) {
                     <p className="cardsTextColor mb-1"><span className="font-semibold">ISBN:</span> {Book.BookItem.isbn}</p>
                     <p className="cardsTextColor mb-3"><span className="font-semibold">Veröffentlicht:</span> {Book.BookItem.publicationDate}</p>
                     <p className="cardsTextColor">{truncateText(Book.BookItem.description, 200)}</p>
+                    <button onClick={addBookToDB}>Zur Bibliothek hinzufügen</button>
                 </div>
             </div>
             </div>
