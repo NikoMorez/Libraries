6dc0da3b43b3d462cbca773fd120274facd67894
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-08-25 15:25:36 +0200

    BookDTO-model for post-requests.

---
+++ frontend/src/models/BookDTO.tsx
@@ -0,0 +1,10 @@
+export type BookDTO = {
+    title: string;
+    author: string;
+    isbn: string;
+    description: string;
+    smallThumbnail : string;
+    thumbnail: string;
+    publicationDate: string;
+    categories: string[];
+}
