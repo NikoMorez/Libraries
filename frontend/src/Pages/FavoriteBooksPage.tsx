9efbec270d729b4072c86c2061b48cfb85d461cb
NeitzelJulia <julia.neitzel86@gmail.com>
2025-08-27 11:57:29 +0200

    add FavoriteBooksPage

---
+++ frontend/src/Pages/FavoriteBooksPage.tsx
@@ -0,0 +1,24 @@
+import BookComponent from "../Components/BookComponent.tsx";
+import type {Book} from "../models/Book.tsx";
+
+type bookloadProp = {
+    books: Book[]
+    onChange: () => void
+}
+
+export default function FavoriteBooksPage({books, onChange} : Readonly<bookloadProp>) {
+
+
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {books.map((book) => (
+
+                <BookComponent key={book.id}
+                               bookItem={book}
+                               onChange={onChange}
+                />
+
+            ))}
+        </div>
+    );
+}
